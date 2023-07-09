64eacae98b90960346ab0611233fb33e9ff9b71b
brendennoblitt <bnoblitt99@gmail.com>
2023-07-09 08:39:18 -0500

    removed api credentials

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+app.listen(5000, () => {
+  console.log("Server is running on http://localhost:5000");
 });
