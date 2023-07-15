1af70b95ae83d53a302459d1bb05511e8a68c852
brendennoblitt <bnoblitt99@gmail.com>
2023-07-15 17:51:11 -0500

    more updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   entry: "./src/main.js",
   output: {
-    path: path.resolve(__dirname, "public"),
+    path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
-  plugins: [new Dotenv()],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+    }),
+    new Dotenv(),
+  ],
 };
