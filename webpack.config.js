f0fa27c8c15a7e3ea50773b329b82c35f2515dcd
brendennoblitt <bnoblitt99@gmail.com>
2023-07-15 17:48:24 -0500

    patch

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
+const path = require("path");
 const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   entry: "./src/main.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
   plugins: [new Dotenv()],
 };
